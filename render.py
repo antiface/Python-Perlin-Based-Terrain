4a341eeb0445614fcfd9b2e3a9fb657298a4434c
GravitatingGnome <GravGnome@gmail.com>
2015-07-17 02:07:44 +0100

    Create render.py

---
+++ render.py
@@ -0,0 +1,31 @@
+import colorsys
+def biomeColour(biome):
+    if biome == "OCEAN":
+        return (0x84,0xa9,0xca)
+    if biome == "ICY PEAKS":
+        return (0xf8,0xf8,0xf8)
+    if biome == "TUNDRA":
+        return (0xdd,0xdd,0xdf)
+    if biome == "TAIGA":
+        return (0xa6,0xba,0xc1)
+    if biome == "MOUNTAIN":
+        return (0x72,0x5e,0x50)
+    if biome == "HIGHLANDS":
+        return (0x65,0x7a,0x47)
+    if biome == "DESERT":
+        return (0xe9,0xdd,0xb6)
+    if biome == "TEMPERATE GRASSLAND":
+        return (0xa8,0xd4,0x89)
+    if biome == "FOREST":
+        return (0x5a,0x97,0x4e)
+    if biome == "SWAMP":
+        return (0x55,0x7d,0x5d)
+    if biome == "MESA":
+        return (0xc8,0x58,0x29)
+    if biome == "SAVANNAH":
+        return (0xb8,0xc3,0x94)
+    if biome == "TROPICAL RAINFOREST":
+        return (0x28,0x7d,0x3b)
+        
+
+           
